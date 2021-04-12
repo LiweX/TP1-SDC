3876aa150f0b0a3b860bd54ea65f0eeb00116a5d
FeedehC <feedehc@gmail.com>
2021-04-12 11:10:50 -0300

    exit failure added

--- main.c
+++ main.c
@@ -6,8 +6,23 @@ void dec_add(int a,int b) __attribute__((cdecl));
 
 int main( int argc, char *argv[] )  {
 
+if( argc == 5 ) {
+    printf("The %d arguments supplied are %s %s %s %s \n",argc-1, argv[1], argv[2],argv[3],argv[4]);
+}
+else {
+    printf("Invalid operands, cast should be: \"./calc <dec/bin> <Operand1> <Operator> <Operand2>\"\n");
+    exit(EXIT_FAILURE);
+}
+   
+/*for (int i = 0; i <= argc; i++) {
+    printf("%d at %p: %s\n", i, argv[i], argv[i]);
+}*/
+
 if (!(strcmp(argv[1], "dec"))){
     printf("Decimal calculator\n");
+}
+if (!(strcmp(argv[1], "bin"))){
+    printf("Binary calculator\n");
 }
 if ((char)argv[3][0]=='+'){
     //printf("Sum\n");
