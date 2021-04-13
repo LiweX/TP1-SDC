9a90c2cff862a6e8413a2d20cfe6c1e126c35f63
Lihue Luna <lihue.luna@mi.unc.edu.ar>
2021-04-13 09:47:23 -0300

    primer test

---
+++ tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+assert_equals () {
+  if [ "$1" = "$2" ]; then
+    echo -e "$Green $Check_Mark La calcu anda gooding and chero banchero $Color_Off"
+  else
+    echo -e "$Red Failed $Color_Off"
+    echo -e "$Red Expected -$1- to equal -$2- $Color_Off"
+    Errors=$((Errors  + 1))
+    exit 1
+  fi
+}
+
+response=$(./calc dec 5 + 5)
+assert_equals "$response" 10
